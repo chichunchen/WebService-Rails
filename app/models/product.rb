cd1df523d38de5eb900830a7c1164e341cd41c40
chi chun chen <chi.chun_chen844@icloud.com>
2014-08-19 19:17:40 +0800

    add Products description and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
     belongs_to :user
+
+    validates :name, :description , :presence => true
+    validates :price, :numericality => { :greater_than_or_equal_to => 1 }
+    validates :name, :uniqueness => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706133621) do
+ActiveRecord::Schema.define(version: 20140819110810) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20140706133621) do
     t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "users", force: true do |t|
