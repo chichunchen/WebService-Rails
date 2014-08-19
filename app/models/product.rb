a4dc2b8a48932ae38c68831b1153d30c550bf5eb
chi chun chen <chi.chun_chen844@icloud.com>
2014-08-20 01:51:14 +0800

    Actice Admin will go wroung  while new and edit product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ActiveRecord::Base
     validates :name, :description , :presence => true
     validates :price, :numericality => { :greater_than_or_equal_to => 1 }
     validates :name, :uniqueness => true
+    # validates_format_of :image_url, :with => %r{\.(gif|jpe?g|png)$}i, :message => "must have an image extension"
 end
