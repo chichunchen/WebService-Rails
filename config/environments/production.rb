128b5b0597b33cf7ef88c49418481e8736336631
maydaycha <maydaychaaaa@gmail.com>
2014-09-27 04:22:29 +0800

    change production mode config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ WebServiceRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: '140.113.72.8:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "netlabtamama@gmail.com",
+    :password => "ilovenetlab",
+  }
 end
